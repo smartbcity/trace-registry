aebd164c9a6e4b1be2529e780809793acb46513a
Luc Heydel <luc@smartb.city>
2023-04-21 18:13:50 +0200

    add some file utils for later

---
+++ platform/api/api-commons/src/jvmMain/kotlin/city/smartb/registry/program/api/commons/utils/FileUtils.kt
@@ -0,0 +1,22 @@
+package city.smartb.registry.program.api.commons.utils
+
+import city.smartb.fs.s2.file.domain.features.command.FileUploadCommand
+import city.smartb.fs.s2.file.domain.model.FilePath
+import kotlinx.coroutines.reactive.awaitLast
+import org.springframework.core.io.buffer.DataBufferUtils
+import org.springframework.http.codec.multipart.FilePart
+import java.io.ByteArrayOutputStream
+
+suspend fun FilePart.contentByteArray(): ByteArray {
+    return ByteArrayOutputStream().use { os ->
+        DataBufferUtils.write(content(), os).awaitLast()
+        os.toByteArray()
+    }
+}
+
+fun FilePath.toUploadCommand() = FileUploadCommand(
+    path = this,
+    metadata = mapOf(
+        "uploadedAt" to System.currentTimeMillis().toString()
+    )
+)
