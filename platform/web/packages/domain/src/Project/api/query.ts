04ec43974ec9c1ef7a31f184c3a78c92591baad2
Odilon <odilon.vidal@icloud.com>
2023-05-03 17:38:55 +0200

    ajout du query

--- platform/web/packages/domain/src/Project/api/query.ts
+++ platform/web/packages/domain/src/Project/api/query.ts
@@ -22,14 +22,3 @@ export const useProjectGetQuery = (params: QueryParams<ProjectGetQuery, ProjectG
     "projectGet", requestProps, params
   )
 }
-
-
-export interface ProjectTransactionPageQuery extends city.smartb.registry.program.f2.project.domain.query.ProjectPageQueryDTO { }
-export interface ProjectTransactionPageResult extends city.smartb.registry.program.f2.project.domain.query.ProjectPageResultDTO { }
-
-export const useProjectTransactionPageQuery = (params: QueryParams<ProjectTransactionPageQuery, ProjectTransactionPageResult>) => {
-  const requestProps = useNoAuthenticatedRequest()
-  return useQueryRequest<ProjectTransactionPageQuery, ProjectTransactionPageResult>(
-      "projectPage", requestProps, params
-  )
-}
