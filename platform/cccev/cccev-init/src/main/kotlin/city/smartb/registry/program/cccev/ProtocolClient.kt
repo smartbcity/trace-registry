9817ff8dfa0b0b755422964dcfcc674562dc4d88
Luc Heydel <luc@smartb.city>
2023-05-05 15:21:40 +0200

    complete init

--- platform/cccev/cccev-init/src/main/kotlin/city/smartb/registry/program/cccev/ProtocolClient.kt
+++ platform/cccev/cccev-init/src/main/kotlin/city/smartb/registry/program/cccev/ProtocolClient.kt
@@ -8,6 +8,7 @@ import city.smartb.registry.program.cccev.requirement.LocalConsultationRequireme
 import city.smartb.registry.program.cccev.requirement.ReddPlusRequirement
 import city.smartb.registry.program.cccev.requirement.VerraVcsRequirement
 import city.smartb.registry.program.cccev.ver.ActivitiesAxess
+import city.smartb.registry.program.cccev.ver.IndicatorsAxess
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.logging.Logging
 import kotlinx.coroutines.flow.asFlow
@@ -36,6 +37,7 @@ suspend fun initRequirement(url: String) {
             addAll(LocalConsultationRequirements)
             addAll(ReddPlusRequirement)
             addAll(VerraVcsRequirement)
+            add(IndicatorsAxess)
         }.asFlow()
     ).onEach {
         println("Created requirement: ${it.identifier}")
