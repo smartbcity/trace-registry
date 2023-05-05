1f2b409d829de9d23152a965760c01a10b4879de
Odilon <odilon.vidal@icloud.com>
2023-05-05 09:48:34 +0200

    color status

--- platform/web/packages/domain/src/Assets/components/ProjectTransactionsTable/useTransactionsFilters.tsx
+++ platform/web/packages/domain/src/Assets/components/ProjectTransactionsTable/useTransactionsFilters.tsx
@@ -1,11 +1,26 @@
 import { useCustomFilters } from 'components'
-import { FilterComposableField } from '@smartb/g2'
+import {FilterComposableField, Option} from '@smartb/g2'
 import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
-import { AssetPageQuery } from "domain-components";
+import {AssetPageQuery, transactionTypeValues} from "domain-components";
+import {transactionTypeValuestoColor} from "../ProjectTransactionInformations/ProjectTransactionStatus";
 
 export const useTransactionsFilters = () => {
+
     const {t} = useTranslation()
+     const typesOptions = useMemo(
+         () => {
+                    const types: Option[] = []
+                    for (let key in transactionTypeValues) {
+                            types.push({
+                                key: key,
+                                label: t(`projects.assets.${key}`),
+                                color: transactionTypeValuestoColor[key]
+                            })
+
+                    }
+                    return types}, [t])
+
     const filters = useMemo((): FilterComposableField[] => [
         {
             name: 'identifier',
@@ -17,9 +32,11 @@ export const useTransactionsFilters = () => {
         },
         {
             name: 'status',
+            label: t("types"),
             type: 'select',
             params: {
-                label: t("status")
+                options: typesOptions,
+                multiple: true
             }
         }
     ], [t])
