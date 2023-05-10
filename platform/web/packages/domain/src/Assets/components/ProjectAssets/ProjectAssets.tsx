45be93a8d76fa9f6cc29a408b8aa7d8c7757c07a
Odilon <odilon.vidal@icloud.com>
2023-05-10 14:11:47 +0200

    History non pris en compte et données factices supprimées

--- platform/web/packages/domain/src/Assets/components/ProjectAssets/ProjectAssets.tsx
+++ platform/web/packages/domain/src/Assets/components/ProjectAssets/ProjectAssets.tsx
@@ -9,17 +9,15 @@ import {
     useTransactionsFilters,
     ProjectTransactionPage,
     useAssetPageQuery,
-    Transaction,
-    Project
+    Transaction
 } from "domain-components";
 
 export interface ProjectAssetsProps {
-    project?: Project
     isLoading: boolean
 }
 
 export const ProjectAssets = (props: ProjectAssetsProps) => {
-    const { isLoading, project } = props
+    const { isLoading } = props
     const { component, setOffset, submittedFilters } = useTransactionsFilters()
     const pagination = useMemo((): OffsetPagination => ({ offset: submittedFilters.offset ?? Offset.default.offset, limit: submittedFilters.limit ?? Offset.default.limit }), [submittedFilters.offset, submittedFilters.limit])
     const transactions = useAssetPageQuery({
@@ -79,51 +77,7 @@ export const ProjectAssets = (props: ProjectAssetsProps) => {
                             }}
                         />
                     }
-                    page={transactions.data ? transactions.data : {
-                        items: [{
-                            id: "SB1-1-KE-SB5801-4-2022-23924-1840-2017",
-                            date: 1683187352,
-                            vintage: "2023",
-                            quantity: 1000000,
-                            to: "0xe2f2c31bd29a8dc820ef14969abe89461",
-                            type: "Transferred",
-                            from: "0xe2f2c31bd29a8dc820ef14969abe479841320",
-                            unit: "boubou",
-                            poolId: "lala"
-                        },{
-                            id: "SB1-1-KE-SB5801-4-2022-23924-1840-2018",
-                            date: 1683187352,
-                            vintage: "2023",
-                            quantity: 1000000,
-                            to: "0xe2f2c31bd29a8dc820ef14969abe89461",
-                            type: "Issued",
-                            from: "0xe2f2c31bd29a8dc820ef14969abe479841320",
-                            unit: "boubou",
-                            poolId: "lala"
-                        },{
-                            id: "SB1-1-KE-SB5801-4-2022-23924-1840-2019",
-                            date: 1683187352,
-                            vintage: "2023",
-                            quantity: 1000000,
-                            to: "0xe2f2c31bd29a8dc820ef14969abe89461",
-                            type: "Available",
-                            from: "0xe2f2c31bd29a8dc820ef14969abe479841320",
-                            unit: "boubou",
-                            poolId: "lala"
-                        },{
-                            id: "SB1-1-KE-SB5801-4-2022-23924-1840-2020",
-                            date: 1683187352,
-                            vintage: "2023",
-                            quantity: 1000000,
-                            to: "0xe2f2c31bd29a8dc820ef14969abe89461",
-                            type: "Offset",
-                            from: "0xe2f2c31bd29a8dc820ef14969abe479841320",
-                            unit: "boubou",
-                            poolId: "lala"
-                        }
-                            ],
-                        total: 10
-                    }}
+                    page={transactions.data}
                     pagination={pagination}
                     isLoading={transactions.isLoading}
                     onOffsetChange={setOffset}
@@ -139,7 +93,7 @@ export const ProjectAssets = (props: ProjectAssetsProps) => {
             </Stack>
             {
                 selectedTransaction ?
-                    <ProjectTransactionPage isLoading={isLoading} project={project} transaction={selectedTransaction} onBack={onBack}/>
+                    <ProjectTransactionPage isLoading={isLoading} transaction={selectedTransaction} onBack={onBack}/>
                     : ""
             }
         </Stack>
