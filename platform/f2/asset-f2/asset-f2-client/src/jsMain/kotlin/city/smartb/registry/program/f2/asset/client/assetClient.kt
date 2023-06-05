08e2163f87f1b916c2bc18a80c2ab53b8b003407
Adrien Poupard <adrien@smartb.city>
2023-06-05 19:37:29 +0200

    feat: add asset-f2-client and asset-pool-f2-client modules with respective clients feat: implement AssetClient and AssetPoolClient with their respective functions feat: add support for accessToken and urlBase in assetClient and assetPoolClient functions

---
+++ platform/f2/asset-f2/asset-f2-client/src/jsMain/kotlin/city/smartb/registry/program/f2/asset/client/assetClient.kt
@@ -0,0 +1,21 @@
+package city.smartb.registry.program.f2.asset.client
+
+import f2.client.F2Client
+import f2.client.ktor.F2ClientBuilder
+import f2.client.ktor.get
+import f2.dsl.fnc.F2SupplierSingle
+
+import f2.dsl.fnc.f2SupplierSingle
+import kotlinx.coroutines.await
+
+@JsName("assetClient")
+@JsExport
+actual fun assetClient(urlBase: String, accessToken: String): F2SupplierSingle<AssetClient> = f2SupplierSingle {
+    F2ClientBuilder.get(urlBase)
+        .await()
+        .let(::AssetClient)
+}
+
+actual fun F2Client.assetClient(): F2SupplierSingle<AssetClient> = f2SupplierSingle {
+    AssetClient(this)
+}
