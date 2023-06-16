b571fe1586103d9b70f7da80e14cf120c4229199
Adrien Poupard <adrien@smartb.city>
2023-06-16 10:35:24 +0200

    refactor(ProjectFakeBuilder.kt): remove unused createYahuma function and related code refactor(init.kt): uncomment createYahuma call in main function

--- platform/cccev/cccev-init/src/main/kotlin/city/smartb/registry/program/cccev/ProjectFakeBuilder.kt
+++ platform/cccev/cccev-init/src/main/kotlin/city/smartb/registry/program/cccev/ProjectFakeBuilder.kt
@@ -66,15 +66,6 @@ class ProjectFakeBuilder(url: String, accessToken: String) {
     )
 }
 
-fun createYahuma(url: String, accessToken: String): Unit = runBlocking {
-    val helper = ProjectFakeBuilder(url, accessToken)
-    val projectClient = helper.projectClient.invoke()
-    val activityClient = helper.activityClient.invoke()
-    val created = projectClient.projectCreate().invoke(flowOf(yahuma())).toList()
-    val project = created.first()
-
-    fullFillProject(project.id, projectClient, activityClient)
-}
 fun createBrazilRockFeller(
     url: String,
     accessToken: String,
@@ -94,8 +85,6 @@ fun createBrazilRockFeller(
 
     val created = projectClient.projectCreate().invoke(flowOf(brazilRockFeller())).toList()
     val project = created.first()
-
-    fullFillProject(project.id, projectClient, activityClient)
 
     createAssetPool(project.id, assetPoolClient, assetClientRockfeller, assetClientOffseter)
 }
@@ -209,50 +198,6 @@ private fun randomProject(
     sdgs = (1..15).shuffled().take((1..15).random())
 )
 
-
-private fun yahuma(): ProjectCreateCommand {
-    val formatter = DateTimeFormatter.ofPattern("dd/MM/yy").withZone(ZoneOffset.UTC).withChronology(IsoChronology.INSTANCE).withDecimalStyle(DecimalStyle.STANDARD)
-    val creditingPeriodStartDate = LocalDate.parse("16/12/20", formatter).toEpochSecond(LocalTime.MIN, ZoneOffset.UTC)
-    val creditingPeriodEndDate = LocalDate.parse("15/12/50", formatter).toEpochSecond(LocalTime.MIN, ZoneOffset.UTC)
-    return ProjectCreateCommand(
-        identifier = "yahumasud",
-        name = "Yahuma Sud",
-        country = "Congo [DRC]",
-        indicator = "carbon",
-        subContinent = "Africa",
-        creditingPeriodStartDate = creditingPeriodStartDate,
-        creditingPeriodEndDate = creditingPeriodEndDate,
-        description = """
-            The Yahuma Sud REDD+ project is located in the DRC, Province of Tshopo on a territory of more than 288,000 hectares. Its main activity is to avoid planned deforestation in the context of a logging concession in favor of improved forest management practices and conservation activities.
-            The financing of the project is ensured by the sale of Verified Carbon Units (VCU) by the Verra VCS standard.
-            The socio-environmental benefits of forest conservation as well as additional activities of cooperation for the sustainable development of local communities will be labeled by the CCB standard of Verra. The project will consolidate an important territorial link in the integrity of the forests of the Province where existing neighboring projects will also benefit from constituting a natural landscape and larger continuous habitats due to the ecological management of Yahuma Sud.
-            These impulses will enable the approximately 75,000 estimated inhabitants of the territory to perpetuate in the long term an open-up anchorage in their forest territory.
-            By reducing Emissions from deforestation and forest degradation, it optimizes synergies between the three Earth Summit Conventions.
-            Indeed, this project based on the results of carbon sequestration, mitigates climate change, protects the biodiversity of the most diverse continental ecosystem, protects the most fragile soils against degradation, while ensuring income for local communities among the most marginalized on the planet.
-            It falls within the context of the REDD+ initiative, like the Emission Reduction Program (ERP) initiated by the Forest Carbon Partnership Facility (FCPF) in the Province of Mai-Ndombe in the DRC.
-        """.trimIndent(),
-        dueDate = null,
-        estimatedReduction = null,
-        localization = "",
-        proponent = OrganizationRef(
-            id = "",
-            name = "Certi Congo"
-        ),
-        type = 1,
-        referenceYear = null,
-        registrationDate = null,
-        vintage = null,
-        slug = null,
-        assessor = null,
-        location = GeoLocation(
-            lon = 24.233000,
-            lat = 0.783000
-        ),
-        vvb = null,
-        activities = listOf("P0", "P1", "P2", "P3", "P4", "P5"),
-        sdgs = emptyList()
-    )
-}
 
 private fun brazilRockFeller(): ProjectCreateCommand {
     val formatter = DateTimeFormatter.ofPattern("dd/MM/yy").withZone(ZoneOffset.UTC).withChronology(IsoChronology.INSTANCE).withDecimalStyle(DecimalStyle.STANDARD)
