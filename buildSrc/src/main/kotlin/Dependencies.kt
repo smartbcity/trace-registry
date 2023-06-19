b8493d4b41a59c505666f05df2cb942faefb0d3b
Luc Heydel <luc@smartb.city>
2023-06-19 10:04:57 +0200

    fix build

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -26,7 +26,7 @@ object Versions {
 	val fs = Framework.connect
 //	val im = Framework.connect
 	val im = "experimental-SNAPSHOT"
-	val cccev = Framework.connect
+	val cccev = "experimental-1962-SNAPSHOT"
 
 	const val bignum = "0.3.8"
 	const val datafaker = "1.8.1"
