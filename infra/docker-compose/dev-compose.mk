d9b7dcea375b1e9de904880a5166d9b11bedfa16
Adrien Poupard <adrien@smartb.city>
2023-08-05 15:12:31 +0200

    chore: Add bclan-init service and .env_dev variable

--- infra/docker-compose/dev-compose.mk
+++ infra/docker-compose/dev-compose.mk
@@ -1,4 +1,4 @@
-DOCKER_COMPOSE_FILE = bclan redis fs kb cccev
+DOCKER_COMPOSE_FILE = bclan redis fs cccev bclan-init
 
 DOCKER_COMPOSE_PATH = infra/docker-compose
 DOCKER_COMPOSE_ENV = $(DOCKER_COMPOSE_PATH)/.env_dev
