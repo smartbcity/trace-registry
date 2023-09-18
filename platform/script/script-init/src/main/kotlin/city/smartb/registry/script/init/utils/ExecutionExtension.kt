fe93d0a29cba2cc73e63eb37df08747c3c1b4da4
Adrien Poupard <adrien@smartb.city>
2023-09-18 14:10:29 +0200

    fix(ImConfig.kt): set default value of refresh_token to null in AccessToken class to improve readability and consistency refactor(InitScript.kt): comment out code related to cccev initialization to improve performance during script execution refactor(InitScript.kt): modify createRandomProject function to create multiple projects and add asset pools to them based on a random condition refactor(init.kt): update nbProject value to 30 to create 30 projects instead of 1 refactor(ProjectFactory.kt): remove unused imports and update createRandomProject function to return a ProjectCreateCommand object instead of a ProjectId refactor(ProjectFactory.kt): modify randomProject function to generate a type based on the count value refactor(ProjectFactory.kt): modify projectPageQuery function to remove unnecessary println statement feat(utils): add asyncExecution extension function to execute a list of suspend functions asynchronously and return the results The changes were made to improve the code readability, performance, and flexibility. The default value of the refresh_token in the AccessToken class was set to null to improve consistency and make the code more readable. The code related to cccev initialization was commented out in the InitScript.kt file to improve performance during script execution. The createRandomProject function in the InitScript.kt file was modified to create multiple projects and add asset pools to them based on a random condition. The nbProject value in the init.kt file was updated to 30 to create 30 projects instead of 1. Unused imports were removed and the createRandomProject function in the ProjectFactory.kt file was updated to return a ProjectCreateCommand object instead of a ProjectId. The randomProject function in the ProjectFactory.kt file was modified to generate a type based on the count value. The projectPageQuery function in the ProjectFactory.kt file was modified to remove an unnecessary println statement. The asyncExecution extension function was added to the ExecutionExtension.kt file to execute a list of suspend functions asynchronously and return the results.

---
+++ platform/script/script-init/src/main/kotlin/city/smartb/registry/script/init/utils/ExecutionExtension.kt
@@ -0,0 +1,19 @@
+package city.smartb.registry.script.init.utils
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+
+suspend inline fun <T, R> List<T>.asyncExecution(
+    size: Int = 2,
+    crossinline execute: suspend (value: T) -> R
+): List<R> = coroutineScope {
+    chunked(8).mapIndexed { index, chunk ->
+        println("Chunk[$index] size: ${chunk.size}")
+        chunk.map {
+            async {
+                execute(it)
+            }
+        }.awaitAll()
+    }.flatten().toList()
+}
