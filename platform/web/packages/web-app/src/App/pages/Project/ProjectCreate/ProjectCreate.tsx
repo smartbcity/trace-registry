7a6f76e1f0c22793f882872d5f3692bf3a44d97e
Basile <basile@smartb.city>
2023-11-21 15:48:13 +0100

    finished the interface of the project onboarding

--- platform/web/packages/web-app/src/App/pages/Project/ProjectCreate/ProjectCreate.tsx
+++ platform/web/packages/web-app/src/App/pages/Project/ProjectCreate/ProjectCreate.tsx
@@ -1,6 +1,6 @@
 import { Stepper, Section, StepItem, AutoForm, Actions, FormComposableState, autoFormFormatter, CommandWithFile } from '@smartb/g2'
 import { useTranslation } from 'react-i18next'
-import { useMemo, useCallback } from 'react'
+import { useMemo, useCallback, useRef,useEffect } from 'react'
 import { AppPage } from 'template'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import summaryForm from "./projectSummaryForm.json"
@@ -22,6 +22,16 @@ export const ProjectCreate = () => {
     const { t } = useTranslation()
     const {projectsCreateStep} = useRoutesDefinition()
     const navigate = useNavigate()
+
+    const setpsCommands = useRef<CommandWithFile<any>[]>([])
+
+    useEffect(
+      () => {
+        if (activeStep > 0) navigate(projectsCreateStep("0"))
+      },
+      [],
+    )
+    
 
     const steps = useMemo((): StepItem[] => [{
         key: "summary",
@@ -52,12 +62,14 @@ export const ProjectCreate = () => {
             onClick: formState.submitForm
         }]} 
         />
-    ) , [t, activeStep])
+    ), [t, activeStep])
 
     const onSubmit = useCallback(
       (command: CommandWithFile<any>) => {
-        console.log(command)
-        navigate(projectsCreateStep(`${activeStep + 1}`))
+        setpsCommands.current[activeStep] = command
+        if (activeStep !== 2) {
+            navigate(projectsCreateStep(`${activeStep + 1}`))
+        }
       },
       [navigate, activeStep],
     )
@@ -79,6 +91,7 @@ export const ProjectCreate = () => {
                 }}
             >
                 <AutoForm
+                //@ts-ignore
                     formData={autoFormFormatter(forms[activeStep])}
                     onSubmit={onSubmit}
                     getFormActions={getActions}
